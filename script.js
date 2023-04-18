59cda807a3abdf4d5f1947c77846617fe99b100a
Pritam96 <pritammondal96@gmail.com>
2023-04-18 23:17:45 +0530

    Task 4 - GetElementsbyClassname

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ var addItem = document.getElementsByClassName('title');
 console.log(addItem);
 addItem[0].style.fontWeight = 'bold';
 addItem[0].style.color = 'green';
+
+var li = document.getElementsByClassName('list-group-item');
+
+li[2].style.backgroundColor = 'green';
+for (var i = 0; i < li.length; i++) {
+  li[i].style.fontWeight = 'bold';
+}
