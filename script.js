d59adfed54e804b360749c5eaadb4dac14ea9225
Pritam96 <pritammondal96@gmail.com>
2023-04-19 10:04:40 +0530

    Task 6 (Re-Commit) - QuerySelector && QuerySelectorAll

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 // Deliverable - 1
-// querySelectorAll
-var items = document.querySelectorAll('.list-group-item');
-console.log(items);
+// querySelector
+var item2 = document.querySelector('.list-group-item:nth-child(2)');
 
 // making the 2nd item background color to green
-items[1].style.backgroundColor = 'green';
+item2.style.backgroundColor = 'green';
 
+var item3 = document.querySelector('.list-group-item:nth-child(3)');
 // making the 3rd item invisible
-items[2].style.display = 'none';
+item3.style.display = 'none';
 
 // Deliverable - 2
 // changing font color to green for second item
-items[1].style.color = 'green';
+document.querySelectorAll('.list-group-item')[1].style.color = 'green';
 
 // changing odd items background to green
-for (var i = 0; i < items.length; i += 2) {
-  items[i].style.backgroundColor = 'green';
+var oddItems = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for (var i = 0; i < oddItems.length; i++) {
+  oddItems[i].style.backgroundColor = 'green';
 }
