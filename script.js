756020619f78e77a56e5cda8c3c694d74393a67c
Pritam96 <pritammondal96@gmail.com>
2023-04-20 08:55:53 +0530

    Task 9 - Filter

--- script.js
+++ script.js
@@ -5,22 +5,33 @@ form.addEventListener('submit', addItem);
 
 itemList.addEventListener('click', removeItem);
 
+filter.addEventListener('keyup', filterItems);
+
 function addItem(e) {
   e.preventDefault();
   //   console.log('Working');
   var textInput = document.getElementById('item').value;
   console.log(textInput);
 
+  // get description
+  var description = document.getElementById('description').value;
+
   var li = document.createElement('li');
   li.className = 'list-group-item';
-  li.appendChild(document.createTextNode(textInput));
+  li.appendChild(document.createTextNode(textInput + ' : ' + description));
 
   var button = document.createElement('button');
   button.className = 'btn btn-danger btn-sm float-right delete';
   button.appendChild(document.createTextNode('X'));
 
-  //   console.log(button);
+  var editButton = document.createElement('Button');
+  editButton.className = 'btn btn-primary btn-sm float-right mr-2 edit';
+  editButton.appendChild(document.createTextNode('Edit'));
+
   li.appendChild(button);
+
+  // adding Edit button
+  button.parentNode.insertBefore(editButton, button.nextSibling);
 
   itemList.appendChild(li);
 }
@@ -37,13 +48,29 @@ function removeItem(e) {
   }
 }
 
-var editButton = document.createElement('Button');
-editButton.className = 'btn btn-primary btn-sm float-right mr-2 edit';
-editButton.appendChild(document.createTextNode('Edit'));
-// console.log(editButton);
+// adding Edit Buttons
+const deleteButtons = document.querySelectorAll('.delete');
 
-var list = document.querySelectorAll('.list-group-item');
-console.log(list);
-for (var i = 0; i < list.length; i++) {
-  list[i].appendChild(editButton);
+deleteButtons.forEach((button) => {
+  // Create a new edit button
+  var editButton = document.createElement('Button');
+  editButton.className = 'btn btn-primary btn-sm float-right mr-2 edit';
+  editButton.appendChild(document.createTextNode('Edit'));
+
+  // Insert the edit button after the delete button
+  button.parentNode.insertBefore(editButton, button.nextSibling);
+});
+
+function filterItems(e) {
+  var text = e.target.value.toLowerCase();
+  var items = itemList.getElementsByTagName('li');
+  // convert to an array
+  Array.from(items).forEach((item) => {
+    var itemName = item.firstChild.textContent.toLowerCase();
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
